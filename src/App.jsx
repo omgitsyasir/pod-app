140d3943c11eac688b6d88271624d595474f8819
omgitsyasir <yasirmumtazsoomro@proton.me>
2025-10-31 00:14:03 -0500

    Added Collapsible Sidebar Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { LayoutDashboard, Plus, List, Home, CheckSquare } from "lucide-react";
+import { LayoutDashboard, Plus, List, Home, CheckSquare, PanelLeftClose } from "lucide-react";
 import { Routes, Route, Link} from 'react-router-dom';
-import { useState} from 'react';
+import { useState } from 'react';
 
 import Adder from './pages/Adder.jsx';
 import Lister from './pages/Lister.jsx';
 
-function IconSidebar(){
+function IconSidebar({ onTogglePanel }){
   return (
     <nav className="w-20 bg-gray-900 text-white p-4 flex flex-col items-center gap-4">
+      <button 
+        onClick={onTogglePanel}
+        className="p-2 rounded-lg hover:bg-gray-700">
+        <PanelLeftClose className="w-6 h-6"/>
+      </button>
       <Link to="/" className="p-2 rounded-lg hover:bg-gray-700">
         <Home className="h-6 w-6"/>
       </Link>
@@ -20,7 +25,7 @@ function IconSidebar(){
 
 function ListPanel(){
   return(
-    <div className="w-64 bg-gray-800 text-white p-4">
+    <div className="w-55 bg-gray-800 text-white p-4">
       <h2 className="font-bold text-lg mb-4">Lists</h2>
       <ul>
         <li className="p-2 rounded-lg hover:bg-gray-700">Work Tasts</li>
@@ -42,10 +47,16 @@ function MainContent(){
 }
 
 export default function App() {
+  const [isPanelOpen, setIsPanelOpen] = useState(true);
+
+  const togglePanel= () => {
+    setIsPanelOpen( (prev) => !prev);
+  };
+
   return(
     <div className="min-h-screen flex flex-row">
-      <IconSidebar/>
-      <ListPanel/>
+      <IconSidebar onTogglePanel={togglePanel}/>
+      {isPanelOpen && <ListPanel/>}
       <MainContent/>
     </div>
 
